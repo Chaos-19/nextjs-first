a28a3082220c54c33f8f29e2aada1fb02eb9b396
Kalkidan Getachew <kalgetachew375@gmail.com>
2024-04-16 15:52:46 -0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import Post, { PostType } from "@/components/Post";
+import getFeeds, { FeedType } from "./api/getFeeds";
 
 export default async function Home() {
+
+  const posts: FeedType = await getFeeds();
 
   return (
     <main className="w-full h-full">
       <div className="p-4">
         <h1 className="text-2xl font-black capitalize">public feed</h1>
         <div className="pt-4">
-          
+          {
+            posts?.feed.map((post) => <Post key={post.id} {...post} />)
+          }
         </div>
       </div>
     </main>
